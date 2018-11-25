bb41c9dc639c6a4a25281001938c1ba274f554b7
Cory Loughran <loughrancory@gmail.com>
2018-11-25 15:10:13 -0400

    finishScript implemented

--- Assets/scripts/FinishScript.cs
+++ Assets/scripts/FinishScript.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinishScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public static int finishAmount = scoreScript.coinAmount;
+    Text score;
+
+    // Use this for initialization
+    void Start () {
+        score = GetComponent<Text>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        score.text = "Your ending coin balance is: " + finishAmount;
+    }
 }
