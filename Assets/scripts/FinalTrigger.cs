0fcb89d34cad5a2062efd321bd90474fb6a18ccf
DanielAyoung <45313999+DanielAyoung@users.noreply.github.com>
2018-11-25 12:50:07 -0400

    Daniel

---
+++ Assets/scripts/FinalTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinalTrigger : MonoBehaviour {
+   [SerializeField] private string LoadLevel;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(LoadLevel);
+                    
+        }
+    }
+
+}
