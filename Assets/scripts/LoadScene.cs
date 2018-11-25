2e7b5b4cb863b8e9f6ce5989e8c2d62b326ef1c3
DanielAyoung <45313999+DanielAyoung@users.noreply.github.com>
2018-11-25 13:01:35 -0400

    Daniel

--- Assets/scripts/LoadScene.cs
+++ Assets/scripts/LoadScene.cs
@@ -7,6 +7,8 @@ using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour {
     public void loadSceneByIndex (int index) {
         SceneManager.LoadScene(index);
+
         SceneManager.LoadScene("Scene2", LoadSceneMode.Additive);
+
     }
 }
