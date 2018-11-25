470169ced499965d19823431e67b3a010ceb477f
Cory Loughran <loughrancory@gmail.com>
2018-11-25 12:57:58 -0400

    Menu loads scene2 as well

--- Assets/scripts/LoadScene.cs
+++ Assets/scripts/LoadScene.cs
@@ -7,5 +7,6 @@ using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour {
     public void loadSceneByIndex (int index) {
         SceneManager.LoadScene(index);
+        SceneManager.LoadScene("Scene2", LoadSceneMode.Additive);
     }
 }
